3927b007042b1379229fd9598ac56e198b6e211b
mizomico <aroky.me@gmail.com>
2026-05-21 23:48:35 +0300

    updating project

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,12 +3,18 @@ import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
   const t = await getTranslations("Home");
+
   return (
     <div>
       <ThemeToggle />
       <div>
+        <nav className="flex justify-between mb-4">
+          <p>left</p>
+          <p>right</p>
+        </nav>
         <h1>{t("title")}</h1>
         <p>{t("description")}</p>
+        <h3>{t("watermelon")}</h3>
       </div>
     </div>
   );
