d3e4a20caea626c7c3faa2a2da379e2e92b43161
maiaboelmagd <maishalan00000@gmail.com>
2026-05-31 20:55:17 +0300

    refactor: improve auction cards responsive layout

--- components/reusable_components/card/AuctionGrid.tsx
+++ components/reusable_components/card/AuctionGrid.tsx
@@ -9,7 +9,7 @@ export default function AuctionGrid({ auctions }: AuctionGridProps) {
   return (
     <div
       dir="rtl"
-      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 gap-4 justify-items-center w-full mx-auto px-2 py-8"
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 gap-4 w-full mx-auto px-4 py-8"
     >
       {auctions.map((auction) => (
         <AuctionCard key={auction.id} {...auction} />
