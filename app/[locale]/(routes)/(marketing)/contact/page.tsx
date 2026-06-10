99d3f29f226e4ef87988bf52046d4b52bb95494f
mizomico <aroky.me@gmail.com>
2026-06-10 14:50:29 +0300

    Edit 2

--- app/[locale]/(routes)/(marketing)/contact/page.tsx
+++ app/[locale]/(routes)/(marketing)/contact/page.tsx
@@ -1,4 +1,4 @@
-//(Abanob) 
+//(Abanob)
 "use client";
 import TextUnderLine from "@/components/Layout/Footer/TextUnderLine";
 import ContactFormView from "@/features/contact/components/ContactFormView";
@@ -8,13 +8,16 @@ export default function ContactPage() {
   return (
     <div className="w-full h-full  px-3 md:px-20 py-3">
       <header className="mb-2 md:mb-5">
-    <TextUnderLine text="تواصل معنا" fontSize="text-2xl md:text-3xl font-semibold"/>
+        <TextUnderLine
+          text="تواصل معنا"
+          fontSize="text-2xl md:text-3xl font-semibold"
+        />
       </header>
-    <FormLayout borderRadius={20}>
-      <div className="bg-white/10 backdrop-blur-lg rounded-3xl px-5 py-7">
-      <ContactFormView />
-      </div>
-    </FormLayout>
+      <FormLayout borderRadius={20}>
+        <div className="bg-white/10 backdrop-blur-lg rounded-3xl px-5 py-7">
+          <ContactFormView />
+        </div>
+      </FormLayout>
     </div>
   );
 }
