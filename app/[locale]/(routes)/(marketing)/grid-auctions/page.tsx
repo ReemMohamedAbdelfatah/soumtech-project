81d8caa67e843385dc183b2e4a563b99df43ce59
mizomico <aroky.me@gmail.com>
2026-06-15 21:35:17 +0300

    Setup Branch

--- app/[locale]/(routes)/(marketing)/grid-auctions/page.tsx
+++ app/[locale]/(routes)/(marketing)/grid-auctions/page.tsx
@@ -1,4 +1,4 @@
-import Display from "@/components/reusable_components/AuctionTable/Display"
+import Display from "@/components/reusable_components/AuctionTable/Display";
 
 export default async function GridAuction(props: any) {
   const searchParams = await props.searchParams;
